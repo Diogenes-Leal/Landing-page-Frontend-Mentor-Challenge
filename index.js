a7af114f1235ad2184d32efd9b12c579962a9ecd
Diógenes <diogenesferraz10@hotmail.com>
2023-03-09 11:50:40 -0300

    Hamburguer menu finished
    Hamburger menu for mobile version is function and is polished.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function toggleMenu(){
     const menu = document.getElementById('id-menu-temporario');
     
-    if(menu.style.display=='none'){
-        menu.style.display='flex';
+    if(menu.style.opacity == 0){
+        menu.style.opacity = 1;
+        menu.style.pointerEvents = 'all';
     } else {
-        menu.style.display='none';
+        menu.style.opacity = 0;
+        menu.style.pointerEvents = 'none';
     }
 }
