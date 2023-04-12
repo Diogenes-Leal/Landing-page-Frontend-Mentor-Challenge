c948bb6b0baacb37a144e1f55943a0a60c5c3230
Diógenes <diogenesferraz10@hotmail.com>
2023-04-12 14:45:07 -0300

    Footer for desktop done
    Footer is working properly.
    Next step is check for problems and fix them.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+function fetchSvg(image, id) {
+    fetch(image.src)
+    .then((response) => response.text())
+    .then((response) => {
+        const span = document.createElement('span');
+        span.innerHTML = response;
+        const inlineSvg = span.getElementsByTagName('svg')[id];
+        image.parentNode.replaceChild(inlineSvg, image);
+        return true;
+    });
+}
+
+function onMouseEnter(tag){
+    getPath(tag).style.fill = "#ffffff";
+}
+
+function onMouseLeave(tag){
+    getPath(tag).style.fill = "#2C7566";
+}
+
+function getPath(tag){
+    const svg = tag.children[0];
+    const path = svg.children[0];
+    return path;
+}
+
 function toggleMenu(){
     const menu = document.getElementById('id-menu-temporario');
     
